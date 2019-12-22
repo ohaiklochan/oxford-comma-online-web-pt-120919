cb8fa46a18cc32eb228bc79e752528489e589e7a
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 20:23:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
     array [-2] << " and "
     array.join
   elsif array.size > 2
-  array.join ", " + ", " + array [-2] " and "
+  array.join ", " + ", and " + array [-1] 
   end
 end
