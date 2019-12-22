6545dcbc5fd00cbdc7f7bde5e5ef55c8646d8626
Chloe Foronda <caforonda103@yahoo.com>
2019-12-22 20:26:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array [-2] << " and "
+    array.join
+  else array.size > 2
+    array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
